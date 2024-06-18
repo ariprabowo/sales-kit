c0874104a88b039e47994bcc8e9121362e83fae2
ariprabowo <ariprabowo95@gmail.com>
2024-06-18 21:44:04 +0700

    default vite config

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,8 @@ import laravel from 'laravel-vite-plugin';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
-import fs from 'fs';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
-    server: {
-      https: {
-        key: fs.readFileSync(process.env.VITE_SSL_KEY),
-        cert: fs.readFileSync(process.env.VITE_SSL_CERT)
-      },
-      proxy: {
-        // Match requests starting with '/resources/' (adjust as needed)
-        '/resources/': {
-          target: 'https://aionsales.id:5173', // Target URL
-          changeOrigin: true, // Optional: Change origin to match development server
-          secure: false, // Optional: Disable if target doesn't use HTTPS
-        },
-      },
-    },
     plugins: [
 
         vue(),
